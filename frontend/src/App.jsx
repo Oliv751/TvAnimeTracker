03f8344fe6f6465e0bfba862c0ffacced708e504
Oliv751 <olivierlopez751@gmail.com>
2023-02-01 13:02:29 +0100

    header style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou de APP</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { MdAccountCircle } from "react-icons/md";
+import { Link } from "react-router-dom";
 import SamuraiLogo from "../assets/SamuraiLogo.png";
+import "../styles/header.scss";
 
 function Header() {
   return (
-    <div>
-      <img className="SamuraiLogo" src={SamuraiLogo} alt="SamuraiLogo" />
-      <h1>Header</h1>
-      <button type="button" className="button-connexion">
-        <MdAccountCircle className="account-icon" />
-      </button>
-    </div>
+    <header className="header">
+      <nav>
+        <div className="full-logo">
+          <Link to="/">
+            <img className="samuraiLogo" src={SamuraiLogo} alt="SamuraiLogo" />
+          </Link>
+          <h1 className="site-title">TvAnimeTracker</h1>
+        </div>
+        <button type="button" className="button-connexion">
+          <MdAccountCircle className="account-icon" />
+        </button>
+      </nav>
+    </header>
   );
 }
 
