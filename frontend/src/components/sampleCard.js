8aadcd14276f4fecf9047e22ba3b947b027b26ae
Oliv751 <olivierlopez751@gmail.com>
2023-02-01 15:51:26 +0100

    create showCard , and import it to showList then to home, and styled it

--- frontend/src/components/sampleCard.js
+++ frontend/src/components/sampleCard.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
 const SampleCard = PropTypes.shape({
+  img: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-  img: PropTypes.shape({ url: PropTypes.string }),
 }).isRequired;
 
 export default SampleCard;
