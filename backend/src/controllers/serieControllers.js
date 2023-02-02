1b2840b42e182b7c568ea1aeedb7b8829164347c
Oliv751 <olivierlopez751@gmail.com>
2023-02-02 17:02:23 +0100

    update database, controllers, manager in backend and SerieDetailsPage with Romain

--- backend/src/controllers/serieControllers.js
+++ backend/src/controllers/serieControllers.js
@@ -14,7 +14,7 @@ const browse = (req, res) => {
 
 const read = (req, res) => {
   models.serie
-    .find(req.params.id)
+    .findForUser(req.params.id, 1)
     .then(([rows]) => {
       if (rows[0] == null) {
         res.sendStatus(404);
